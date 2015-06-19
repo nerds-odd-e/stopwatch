2194b3cf34a4dc98f2c7c99c0c9bd93b034fe7d2
Asuka <asukadi@163.com>
2015-06-19 16:42:20 +0800

    增加恢复计时场景

--- features/step_definitions/start_and_stop_steps.rb
+++ features/step_definitions/start_and_stop_steps.rb
@@ -5,7 +5,7 @@
 end
 
 当(/^经过(\d+)秒$/) do |second|
-    sleep second.to_i + 0.5
+    sleep second.to_i + 0.2
 end
 
 那么(/^看见显示(\d+)秒$/) do |second|
@@ -14,10 +14,20 @@ end
 
 
 当(/^经过(\d+)秒，用户暂停秒表$/) do |second|
-  sleep second.to_i + 0.5
+  step "经过#{second}秒"
   tap_mark 'Pause'
 end
 
 当(/^等待(\d+)秒$/) do |second|
-  sleep second.to_i + 0.5
+    step "经过#{second}秒"
+end
+
+假如(/^秒表计时到(\d+)秒后暂停$/) do |second|
+  step "用户启动秒表"
+  step "经过#{second}秒，用户暂停秒表"
+end
+
+当(/^经过(\d+)秒，用户恢复计时$/) do |second|
+  step "等待#{second}秒"
+  step "用户启动秒表"
 end
