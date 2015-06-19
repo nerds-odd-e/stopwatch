5d4eaef1a7f05d538ef48a5cbb8323b52c646722
Asuka <asukadi@163.com>
2015-06-19 15:22:36 +0800

    启动秒表并计时

--- app/src/main/java/com/bodong/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/bodong/stopwatch/StopwatchActivity.java
@@ -2,13 +2,14 @@ package com.bodong.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 
 public class StopwatchActivity extends Activity {
 
     TextView tv;
-    Stopwatch mStopwatch = new Stopwatch();
+    Stopwatch mStopwatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +18,20 @@ public class StopwatchActivity extends Activity {
 
         tv = (TextView) findViewById(R.id.textview);
 
-        new UIRefresher(this).startTimer();
+
     }
 
     public void updateUI() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                tv.setText(mStopwatch.secondPassed()+"s");
+                tv.setText(mStopwatch.secondPassed() + "s");
             }
         });
+    }
+
+    public void start(View view){
+        mStopwatch = new Stopwatch();
+        new UIRefresher(this).startTimer();
     }
 }
--- features/step_definitions/start_and_stop_steps.rb
+++ features/step_definitions/start_and_stop_steps.rb
@@ -1,10 +1,11 @@
 # language: zh-CN
 
-假如(/^用户打开应用$/) do
+假如(/^用户启动秒表$/) do
+    tap_mark 'Start'
 end
 
 当(/^经过(\d+)秒$/) do |second|
-    sleep second.to_i
+    sleep second.to_i + 0.5
 end
 
 那么(/^看见显示(\d+)秒$/) do |second|
